e6690d78b05f938aebbecab8ab69c52109077e30
Andrew Thomas <andrew@andrewrthomas.com>
2025-03-06 16:16:31 -0800

    schtuff

--- src/btree.rs
+++ src/btree.rs
@@ -6,7 +6,7 @@
 
 */
 
-use std::{collections::HashMap, fmt::Display};
+use std::collections::HashMap;
 
 /// B+ Tree
 pub struct BTree<K, V>
@@ -43,62 +43,82 @@ where
         }
     }
 
-    pub fn get(&self, key: K) -> Option<&V> {
-        let mut current_page = self.pages.get(&self.root).unwrap();
-
-        'trav: loop {
-            match current_page {
-                Page::Leaf { keys, vals } => {
-                    for (k, i) in keys.iter().zip(0..) {
-                        if *k == key {
-                            return Some(&vals[i]);
-                        }
-                    }
-                    return None;
+    pub fn get(&self, key: &K) -> Option<&V> {
+        let page_id = self.find_leaf_page(key);
+        let page = self.pages.get(&page_id).unwrap();
+        if let Page::Leaf { keys, vals } = page {
+            for (k, i) in keys.iter().zip(0..) {
+                if k == key {
+                    return Some(&vals[i]);
                 }
-                Page::Inner { keys, pointers } => {
-                    for (k, i) in keys.iter().zip(0..) {
-                        if key < *k {
-                            current_page = self.pages.get(&pointers[i]).unwrap();
-                            continue 'trav;
-                        }
-                    }
-                    current_page = self.pages.get(pointers.last().unwrap()).unwrap();
+            }
+        }
+        None
+    }
+
+    pub fn insert(&mut self, key: K, val: V) {
+        let page_id = self.find_leaf_page(&key);
+        let page = self.pages.get_mut(&page_id).unwrap();
+        if let Page::Leaf { keys, vals } = page {
+            if keys.len() >= self.page_cap {
+                // TODO: gonna need to do a split
+            }
+            let mut idx = None;
+            for (k, i) in keys.iter().zip(0..) {
+                if key < *k {
+                    idx = Some(i);
+                    break;
+                }
+            }
+            match idx {
+                Some(i) => {
+                    keys.insert(i, key);
+                    vals.insert(i, val);
+                }
+                None => {
+                    keys.push(key);
+                    vals.push(val);
                 }
             }
         }
     }
 
-    pub fn set(&mut self, key: K, val: V) {
-        let mut current_page_id = self.root;
-        'trav: loop {
-            let current_page = self.pages.get_mut(&current_page_id).unwrap();
-            match current_page {
-                Page::Inner { keys, pointers } => {
-                    for (k, i) in keys.iter().zip(0..) {
-                        if key < *k {
-                            current_page_id = pointers[i];
-                            continue 'trav;
-                        }
+    /// this method does not do tree rebalancing when an underflow occurs
+    /// it will instead rely on future insertions or vaccuum operations to
+    /// do the rebalancing
+    pub fn remove(&mut self, key: &K) {
+        let page_id = self.find_leaf_page(key);
+        let page = self.pages.get_mut(&page_id).unwrap();
+        if let Page::Leaf { keys, vals } = page {
+            let i = {
+                let mut out = None;
+                for (k, i) in keys.iter().zip(0..) {
+                    if k == key {
+                        out = Some(i);
+                        break;
                     }
-                    current_page_id = *pointers.last().unwrap();
                 }
-                Page::Leaf { keys, vals } => {
-                    for (k, i) in keys.iter().zip(0..) {
-                        if key < *k {
-                            // do insert
-                            keys.insert(i, key);
-                            vals.insert(i, val);
-                            break 'trav;
-                        }
-                    }
-                    // insert at end
-                    keys.push(key);
-                    vals.push(val);
-                    break 'trav;
+                out
+            };
+            if let Some(idx) = i {
+                keys.remove(idx);
+                vals.remove(idx);
+            }
+        }
+    }
+
+    fn find_leaf_page(&self, key: &K) -> usize {
+        let mut current_page_id = self.root;
+        'trav: while let Page::Inner { keys, pointers } = self.pages.get(&current_page_id).unwrap()
+        {
+            for (k, i) in keys.iter().zip(0..) {
+                if key < k {
+                    current_page_id = pointers[i];
+                    continue 'trav;
                 }
             }
         }
+        current_page_id
     }
 }
 
@@ -109,7 +129,13 @@ mod tests {
     #[test]
     fn basic_set_get() {
         let mut btree = BTree::<&'static str, u32>::new(5);
-        btree.set("one", 1);
-        assert_eq!(btree.get("one"), Some(&1));
+        btree.insert("one", 1);
+        btree.insert("two", 2);
+        btree.insert("three", 3);
+        btree.insert("four", 4);
+        assert_eq!(btree.get(&"one"), Some(&1));
+        btree.remove(&"one");
+        assert_eq!(btree.get(&"one"), None);
+        assert_eq!(btree.get(&"three"), Some(&3));
     }
 }
