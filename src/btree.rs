5f87b6e3d41655a4df128cf980f15fc229c1bf0a
Andrew Thomas <andrew@andrewrthomas.com>
2025-04-11 15:18:27 -0700

    who needs types anyway

--- src/btree.rs
+++ src/btree.rs
@@ -5,16 +5,16 @@
 
     TODO:
     - write docs, including the structure of the pages
+    - sibling pointers
 */
 
 use std::{
-    any::Any,
     collections::BTreeMap,
     sync::{Arc, atomic::Ordering},
 };
 
 use crate::{
-    buffer::{PageBuffer, ReserveResult},
+    buffer::{PageBuffer, ReserveResult, SealGuard},
     mapping_table::{FrameInner, Table},
     page_table::PageId,
 };
@@ -24,151 +24,144 @@ const B: usize = 1024; // size of the mapping table blocks
 const PAGE_CAP: usize = 1024; // physical capacity of the pages
 const SPLIT_POINT: usize = 512; // physical size at which a split is triggered
 
-/// btree cursors are owned by a single thread, and are not concurrently
-/// accessed, they just encode the btree logic
-pub struct BTreeCursor {
-    table: Arc<Table<B>>,
-    // TODO: we probably want to include the actual buffer here if possible
-    path: Vec<PageId>,
+pub fn get(table: &Table<B>, key: &[u8], out: &mut Vec<u8>, path: &mut Vec<PageId>) -> Option<()> {
+    find_leaf(table, key, path);
+    let leaf = table.read(*path.last().unwrap()).read();
+    match search_leaf(leaf, key) {
+        Some(val) => {
+            // PERF: some kind of memcpy at least
+            for v in val {
+                out.push(*v);
+            }
+            return Some(());
+        }
+        None => None,
+    }
 }
-
-impl BTreeCursor {
-    /// out is expected to be cleared and have some space allocated already
-    pub fn get(&mut self, key: &[u8], out: &mut Vec<u8>) -> Option<()> {
-        self.find_leaf(key);
-        let leaf = self.table.read(*self.path.last().unwrap()).read();
-        match search_leaf(leaf, key) {
-            Some(val) => {
-                // PERF: some kind of memcpy at least
-                for v in val {
-                    out.push(*v);
-                }
-                return Some(());
-            }
-            None => None,
+pub fn set(table: &Table<B>, key: &[u8], val: &[u8], path: &mut Vec<PageId>) -> Result<(), ()> {
+    find_leaf(table, key, path);
+    match table
+        .read(path.pop().unwrap())
+        .reserve(9 + key.len() + val.len())
+    {
+        ReserveResult::Ok(write_guard) => {
+            // regular happy path, write the new delta
+            write_guard.write[0] = 1;
+            write_guard.write[1..5].copy_from_slice(&(key.len() as u32).to_be_bytes());
+            write_guard.write[5..9].copy_from_slice(&(val.len() as u32).to_be_bytes());
+            write_guard.write[9..9 + key.len()].copy_from_slice(key);
+            write_guard.write[9 + key.len()..9 + key.len() + val.len()].copy_from_slice(val);
+            Ok(())
         }
-    }
-    pub fn set(&mut self, key: &[u8], val: &[u8]) -> Result<(), ()> {
-        self.find_leaf(key);
-        match self
-            .table
-            .read(self.path.pop().unwrap())
-            .reserve(9 + key.len() + val.len())
-        {
-            ReserveResult::Ok(write_guard) => {
-                // regular happy path, write the new delta
-                write_guard.write[0] = 1;
-                write_guard.write[1..5].copy_from_slice(&(key.len() as u32).to_be_bytes());
-                write_guard.write[5..9].copy_from_slice(&(val.len() as u32).to_be_bytes());
-                write_guard.write[9..9 + key.len()].copy_from_slice(key);
-                write_guard.write[9 + key.len()..9 + key.len() + val.len()].copy_from_slice(val);
-                Ok(())
-            }
-            ReserveResult::Sealed => Err(()), // buffer already sealed, just fail
-            ReserveResult::Sealer(seal_guard) => {
-                // we sealed the buffer, so we need to compact, and might need to split
-                // first, we get a fresh buffer to compact into
-                let new_page = PageBuffer::new(PAGE_CAP);
-                let new_buf = unsafe { new_page.raw_buffer() };
-
-                // then wait for current writers to finish,
-                // and get the old buf back
-                let old_buf = seal_guard.wait_for_writers();
-
-                // do the compaction
-                match compact_leaf(old_buf, new_buf) {
-                    CompactResult::Ok(top) => {
-                        // compaction worked, so we just need to update the
-                        // mapping table
-                        new_page.read_offset.store(top, Ordering::Release);
-                        new_page.write_offset.store(top as isize, Ordering::Release);
-                        self.table
-                            .update(*self.path.last().unwrap(), &FrameInner::Mem(new_page));
-                        // TODO: we would also be recycling the old buffer here
-                        Ok(())
-                    }
-                    CompactResult::NeedsSplit(top, split_idx) => {
-                        // the page is compacted, but needs to be split
-                        let (to_page_id, to_page) = self.table.pop();
-                        let to_buf = unsafe { to_page.raw_buffer() };
-                        // TODO: make sure we use these to update the page buffer atomic indices
-                        let (from_top, to_top) = split_leaf(&mut new_buf[top..], to_buf, split_idx);
-                        // then we find the middle key, which is the right most key of the "to"
-                        // page
-                        let middle_key = {
-                            let num_entries = u16::from_be_bytes(
-                                to_buf[to_top + 1..to_top + 3].try_into().unwrap(),
-                            ) as usize;
-                            let key_len = u32::from_be_bytes(
-                                to_buf[to_top + 3 + ((num_entries - 1) * 8)
-                                    ..to_top + 3 + ((num_entries - 1) * 8) + 4]
-                                    .try_into()
-                                    .unwrap(),
-                            ) as usize;
-                            &to_buf[to_top + 3 + ((num_entries - 1) * 8) + 8
-                                ..to_top + 3 + ((num_entries - 1) * 8) + 8 + key_len]
-                        };
-
-                        // page has been split, now we need to add the new
-                        // page id to the parent, this may trigger another
-                        // compaction and/or split, all the way up the path
-                        let parent_id = self.path.pop().unwrap();
-                        let parent = self.table.read(parent_id);
-                        match parent.reserve(13 + middle_key.len()) {
-                            ReserveResult::Ok(write_guard) => {
-                                write_guard.write[0] = 1;
-                                write_guard.write[1..5]
-                                    .copy_from_slice(&(middle_key.len() as u32).to_be_bytes());
-                                write_guard.write[5..13].copy_from_slice(&to_page_id.to_be_bytes());
-                                write_guard.write[13..].copy_from_slice(middle_key);
-                            }
-                            ReserveResult::Sealed => {
-                                // need to figure out what to do here,
-                                // failure may not be an option, and we might
-                                // need to spin until we get it
-                                todo!()
-                            }
-                            ReserveResult::Sealer(seal_guard) => {
-                                // we need to compact the parent
-                                todo!()
-                            }
-                        }
-                        todo!()
-                    }
-                    CompactResult::NeedsMerge => panic!(),
-                }
-            }
-        }
-    }
-
-    fn find_leaf(&mut self, key: &[u8]) {
-        self.path.clear();
-        let mut page_id = self.table.root.load(Ordering::Acquire);
-        loop {
-            self.path.push(page_id);
-            let page_buffer = self.table.read(page_id).read();
-            if page_buffer[page_buffer.len() - 1] > 0 {
-                // inner page
-                // so we gotta read the page to find the next one to go to
-                page_id = search_inner(page_buffer, key);
-            } else {
-                // leaf page
-                break;
-            }
+        ReserveResult::Sealed => Err(()), // buffer already sealed, just fail
+        ReserveResult::Sealer(seal_guard) => {
+            handle_compaction(table, seal_guard, path);
+            Ok(())
         }
     }
 }
 
-fn split_inner() {
+fn find_leaf(table: &Table<B>, key: &[u8], path: &mut Vec<PageId>) {
+    path.clear();
+    let mut page_id = table.root.load(Ordering::Acquire);
+    loop {
+        path.push(page_id);
+        let page_buffer = table.read(page_id).read();
+        if page_buffer[page_buffer.len() - 1] > 0 {
+            // inner page
+            // so we gotta read the page to find the next one to go to
+            page_id = search_inner(page_buffer, key);
+        } else {
+            // leaf page
+            break;
+        }
+    }
+    todo!()
+}
+fn handle_compaction(table: &Table<B>, seal_guard: SealGuard, path: &mut Vec<PageId>) {
+    // we sealed the buffer, so we need to compact, we also might need to split,
+    // which may go all the way up to the top, so we need a loop
+    // first, we get a fresh buffer to compact into
+    let new_page = PageBuffer::new(PAGE_CAP);
+    let new_buf = unsafe { new_page.raw_buffer() };
+
+    // then wait for current writers to finish,
+    // and get the old buf back
+    let old_buf = seal_guard.wait_for_writers();
+    // do the compaction
+    match compact_leaf(old_buf, new_buf) {
+        CompactResult::Ok(top) => {
+            // compaction worked, so we just need to update the
+            // mapping table
+            new_page.read_offset.store(top, Ordering::Release);
+            new_page.write_offset.store(top as isize, Ordering::Release);
+            table.update(*path.last().unwrap(), &FrameInner::Mem(new_page));
+            // TODO: we would also be recycling the old buffer here
+        }
+        CompactResult::NeedsSplit(top, split_idx) => {}
+        CompactResult::NeedsMerge => panic!(),
+    }
+    todo!()
+}
+fn handle_split(
+    table: &Table<B>,
+    new: &mut [u8],
+    path: &mut Vec<PageId>,
+    top: usize,
+    split_idx: usize,
+) {
+    // the page is compacted, but needs to be split
+    let (to_page_id, to_page) = table.pop();
+    let to_buf = unsafe { to_page.raw_buffer() };
+    // TODO: make sure we use these to update the page buffer atomic indices
+    let (from_top, to_top) = split_leaf(&mut new[top..], to_buf, split_idx);
+    // then we find the middle key, which is the right most key of the "to"
+    // page
+    let middle_key = {
+        let num_entries =
+            u16::from_be_bytes(to_buf[to_top + 1..to_top + 3].try_into().unwrap()) as usize;
+        let key_len = u32::from_be_bytes(
+            to_buf[to_top + 3 + ((num_entries - 1) * 8)..to_top + 3 + ((num_entries - 1) * 8) + 4]
+                .try_into()
+                .unwrap(),
+        ) as usize;
+        &to_buf[to_top + 3 + ((num_entries - 1) * 8) + 8
+            ..to_top + 3 + ((num_entries - 1) * 8) + 8 + key_len]
+    };
+
+    // page has been split, now we need to add the new
+    // page id to the parent, this may trigger another
+    // compaction and/or split, all the way up the path
+    let parent_id = path.pop().unwrap();
+    let parent = table.read(parent_id);
+    match parent.reserve(13 + middle_key.len()) {
+        ReserveResult::Ok(write_guard) => {
+            write_guard.write[0] = 1;
+            write_guard.write[1..5].copy_from_slice(&(middle_key.len() as u32).to_be_bytes());
+            write_guard.write[5..13].copy_from_slice(&to_page_id.to_be_bytes());
+            write_guard.write[13..].copy_from_slice(middle_key);
+        }
+        ReserveResult::Sealed => {
+            // need to figure out what to do here,
+            // failure may not be an option, and we might
+            // need to spin until we get it
+            todo!()
+        }
+        ReserveResult::Sealer(seal_guard) => {
+            // we need to compact the parent
+            todo!()
+        }
+    }
+    todo!()
+}
+
+fn split_inner<'s>(from: &'s mut [u8], to: &'s mut [u8], idx: usize) -> (usize, usize) {
     todo!()
 }
 
 // NOTE:
 // - we always split to the left
 // - we always assume empty buffers are filled with 0s
-//
-// TODO:
-// - get rid of all the offset stuff
 
 /// splits [`from`] "left", with everything less than or equal to [`idx`] going into [`to`],
 /// returns the new top offset of both pages
@@ -211,7 +204,10 @@ fn split_leaf<'s>(from: &'s mut [u8], to: &'s mut [u8], idx: usize) -> (usize, u
     from[from_start + (from_end - from_start) - 2..from_start + (from_end - from_start)]
         .copy_from_slice(&((from_entries - idx) as u16).to_be_bytes());
 
-    todo!()
+    (
+        (to_len - (from_len - from_end)) - (3 + idx * 8) - 1,
+        from_start + (from_end - from_start) - 3,
+    )
 }
 
 /// first usize is the top index for the page
@@ -222,7 +218,46 @@ enum CompactResult {
     NeedsMerge,
 }
 
-fn compact_inner() {
+fn compact_inner(og: &[u8], new: &mut [u8]) -> CompactResult {
+    let mut entries = BTreeMap::new();
+    let mut i = 0;
+    loop {
+        match og[i] {
+            0 => {
+                // base page
+                let num_entries = u16::from_be_bytes(og[i + 1..i + 3].try_into().unwrap()) as usize;
+                let mut cursor = og.len() - 1;
+                for e in 0..num_entries {
+                    let key_len = u32::from_be_bytes(
+                        og[i + 11 + (e * 12)..i + 11 + (e * 12) + 4]
+                            .try_into()
+                            .unwrap(),
+                    ) as usize;
+                    let key = &og[cursor - key_len..cursor];
+                    let page_id = u64::from_be_bytes(
+                        og[i + 11 + (e * 12) + 4..i + 11 + (e * 12) + 12]
+                            .try_into()
+                            .unwrap(),
+                    );
+                    entries.insert(key, page_id);
+                    cursor -= key_len;
+                }
+                break;
+            }
+            1 => {
+                // insup
+                let key_len = u32::from_be_bytes(og[i + 1..i + 5].try_into().unwrap()) as usize;
+                let key = &og[i + 13..i + 13 + key_len];
+                let page_id = u64::from_be_bytes(og[i + 5..i + 13].try_into().unwrap());
+                entries.insert(key, page_id);
+                i += 13 + key_len;
+            }
+            _ => panic!(),
+        }
+    }
+
+    // TODO: we need to figure out how to do the right pointers
+
     todo!()
 }
 
@@ -309,6 +344,7 @@ fn compact_leaf(og: &[u8], new: &mut [u8]) -> CompactResult {
 }
 
 fn search_inner(page_buf: &[u8], key: &[u8]) -> PageId {
+    let mut entries = BTreeMap::new();
     let mut i = 0;
     loop {
         match page_buf[i] {
@@ -324,14 +360,19 @@ fn search_inner(page_buf: &[u8], key: &[u8]) -> PageId {
                             .try_into()
                             .unwrap(),
                     ) as usize;
-                    if key <= &page_buf[cursor - key_len..cursor] {
-                        return u64::from_be_bytes(
-                            page_buf[i + 3 + (e * 12) + 4..i + 3 + (e * 12) + 12]
-                                .try_into()
-                                .unwrap(),
-                        );
-                    }
+                    let k = &page_buf[cursor - key_len..cursor];
+                    let page_id = u64::from_be_bytes(
+                        page_buf[i + 3 + (e * 12) + 4..i + 3 + (e * 12) + 12]
+                            .try_into()
+                            .unwrap(),
+                    );
+                    entries.insert(k, page_id);
                     cursor -= key_len;
+                }
+                for (k, pid) in entries {
+                    if key <= k {
+                        return pid;
+                    }
                 }
 
                 return u64::from_be_bytes(page_buf[i + 3..i + 3 + 8].try_into().unwrap());
@@ -340,12 +381,9 @@ fn search_inner(page_buf: &[u8], key: &[u8]) -> PageId {
                 // insert or update
                 let key_len =
                     u32::from_be_bytes(page_buf[i + 1..i + 5].try_into().unwrap()) as usize;
-                // FIX: this won't work, if we find a delta for an entry super far to the right,
-                // we'd return early even if there's an entry in a lower delta or base page that
-                // would be closer (correct)
-                if key <= &page_buf[i + 5 + 8..i + 5 + 8 + key_len] {
-                    return u64::from_be_bytes(page_buf[i + 5..i + 5 + 8].try_into().unwrap());
-                }
+                let k = &page_buf[i + 5 + 8..i + 5 + 8 + key_len];
+                let page_id = u64::from_be_bytes(page_buf[i + 5..i + 5 + 8].try_into().unwrap());
+                entries.insert(k, page_id);
                 i += 5 + 8 + key_len;
             }
             _ => panic!(),
