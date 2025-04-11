68592a1607e20fffbfd9f333b9fd2e5e9af2fe21
Andrew Thomas <andrew@andrewrthomas.com>
2025-04-11 16:01:18 -0700

    getting there

--- src/btree.rs
+++ src/btree.rs
@@ -8,23 +8,24 @@
     - sibling pointers
 */
 
-use std::{
-    collections::BTreeMap,
-    sync::{Arc, atomic::Ordering},
-};
-
 use crate::{
     buffer::{PageBuffer, ReserveResult, SealGuard},
     mapping_table::{FrameInner, Table},
     page_table::PageId,
 };
 
+use std::{collections::BTreeMap, ops::Range, sync::atomic::Ordering};
+
 // NOTE: these will all be configuarable eventually
-const B: usize = 1024; // size of the mapping table blocks
 const PAGE_CAP: usize = 1024; // physical capacity of the pages
 const SPLIT_POINT: usize = 512; // physical size at which a split is triggered
 
-pub fn get(table: &Table<B>, key: &[u8], out: &mut Vec<u8>, path: &mut Vec<PageId>) -> Option<()> {
+pub fn get<const B: usize>(
+    table: &Table<B>,
+    key: &[u8],
+    out: &mut Vec<u8>,
+    path: &mut Vec<PageId>,
+) -> Option<()> {
     find_leaf(table, key, path);
     let leaf = table.read(*path.last().unwrap()).read();
     match search_leaf(leaf, key) {
@@ -38,7 +39,14 @@ pub fn get(table: &Table<B>, key: &[u8], out: &mut Vec<u8>, path: &mut Vec<PageI
         None => None,
     }
 }
-pub fn set(table: &Table<B>, key: &[u8], val: &[u8], path: &mut Vec<PageId>) -> Result<(), ()> {
+pub fn set<const B: usize>(
+    table: &Table<B>,
+    key: &[u8],
+    val: &[u8],
+    path: &mut Vec<PageId>,
+    leaf_scratch: &mut Vec<(Range<usize>, Range<usize>)>,
+    inner_scratch: &mut Vec<(Range<usize>, PageId)>,
+) -> Result<(), ()> {
     find_leaf(table, key, path);
     match table
         .read(path.pop().unwrap())
@@ -55,13 +63,15 @@ pub fn set(table: &Table<B>, key: &[u8], val: &[u8], path: &mut Vec<PageId>) -> 
         }
         ReserveResult::Sealed => Err(()), // buffer already sealed, just fail
         ReserveResult::Sealer(seal_guard) => {
-            handle_compaction(table, seal_guard, path);
+            handle_compaction(table, seal_guard, path, leaf_scratch, inner_scratch);
+            inner_scratch.clear();
+            leaf_scratch.clear();
             Ok(())
         }
     }
 }
 
-fn find_leaf(table: &Table<B>, key: &[u8], path: &mut Vec<PageId>) {
+fn find_leaf<const B: usize>(table: &Table<B>, key: &[u8], path: &mut Vec<PageId>) {
     path.clear();
     let mut page_id = table.root.load(Ordering::Acquire);
     loop {
@@ -78,7 +88,13 @@ fn find_leaf(table: &Table<B>, key: &[u8], path: &mut Vec<PageId>) {
     }
     todo!()
 }
-fn handle_compaction(table: &Table<B>, seal_guard: SealGuard, path: &mut Vec<PageId>) {
+fn handle_compaction<'c, const B: usize>(
+    table: &Table<B>,
+    seal_guard: SealGuard<'c>,
+    path: &mut Vec<PageId>,
+    leaf_scratch: &mut Vec<(Range<usize>, Range<usize>)>,
+    _inner_scratch: &mut Vec<(Range<usize>, PageId)>,
+) {
     // we sealed the buffer, so we need to compact, we also might need to split,
     // which may go all the way up to the top, so we need a loop
     // first, we get a fresh buffer to compact into
@@ -89,7 +105,7 @@ fn handle_compaction(table: &Table<B>, seal_guard: SealGuard, path: &mut Vec<Pag
     // and get the old buf back
     let old_buf = seal_guard.wait_for_writers();
     // do the compaction
-    match compact_leaf(old_buf, new_buf) {
+    match compact_leaf(old_buf, new_buf, leaf_scratch) {
         CompactResult::Ok(top) => {
             // compaction worked, so we just need to update the
             // mapping table
@@ -98,12 +114,11 @@ fn handle_compaction(table: &Table<B>, seal_guard: SealGuard, path: &mut Vec<Pag
             table.update(*path.last().unwrap(), &FrameInner::Mem(new_page));
             // TODO: we would also be recycling the old buffer here
         }
-        CompactResult::NeedsSplit(top, split_idx) => {}
+        CompactResult::NeedsSplit(..) => todo!(),
         CompactResult::NeedsMerge => panic!(),
     }
-    todo!()
 }
-fn handle_split(
+fn handle_split<const B: usize>(
     table: &Table<B>,
     new: &mut [u8],
     path: &mut Vec<PageId>,
@@ -114,7 +129,7 @@ fn handle_split(
     let (to_page_id, to_page) = table.pop();
     let to_buf = unsafe { to_page.raw_buffer() };
     // TODO: make sure we use these to update the page buffer atomic indices
-    let (from_top, to_top) = split_leaf(&mut new[top..], to_buf, split_idx);
+    let (_, to_top) = split_leaf(&mut new[top..], to_buf, split_idx);
     // then we find the middle key, which is the right most key of the "to"
     // page
     let middle_key = {
@@ -147,7 +162,7 @@ fn handle_split(
             // need to spin until we get it
             todo!()
         }
-        ReserveResult::Sealer(seal_guard) => {
+        ReserveResult::Sealer(_) => {
             // we need to compact the parent
             todo!()
         }
@@ -155,7 +170,7 @@ fn handle_split(
     todo!()
 }
 
-fn split_inner<'s>(from: &'s mut [u8], to: &'s mut [u8], idx: usize) -> (usize, usize) {
+fn split_inner<'s>(_from: &'s mut [u8], _to: &'s mut [u8], _idx: usize) -> (usize, usize) {
     todo!()
 }
 
@@ -218,7 +233,7 @@ enum CompactResult {
     NeedsMerge,
 }
 
-fn compact_inner(og: &[u8], new: &mut [u8]) -> CompactResult {
+fn compact_inner(og: &[u8], _new: &mut [u8]) -> CompactResult {
     let mut entries = BTreeMap::new();
     let mut i = 0;
     loop {
@@ -261,13 +276,13 @@ fn compact_inner(og: &[u8], new: &mut [u8]) -> CompactResult {
     todo!()
 }
 
-fn compact_leaf(og: &[u8], new: &mut [u8]) -> CompactResult {
+fn compact_leaf<'c>(
+    og: &'c [u8],
+    new: &mut [u8],
+    scratch: &mut Vec<(Range<usize>, Range<usize>)>,
+) -> CompactResult {
     // for now we will assume that there's enough physical space in the new
     // buffer for everything, but that may not be true in some cases
-
-    // FIX: this is definitely not the right way to do this, but just trying
-    // to get a sense of the logic first
-    let mut entries = BTreeMap::new();
 
     // first we collect all the entries in key order
     let mut i = 0;
@@ -286,9 +301,10 @@ fn compact_leaf(og: &[u8], new: &mut [u8]) -> CompactResult {
                             .try_into()
                             .unwrap(),
                     ) as usize;
-                    let key = &og[cursor - (key_len + val_len)..cursor - val_len];
-                    let val = &og[cursor - val_len..cursor];
-                    entries.insert(key, val);
+                    scratch.push((
+                        cursor - (key_len + val_len)..cursor - val_len,
+                        cursor - val_len..cursor,
+                    ));
                     cursor -= key_len + val_len;
                 }
                 break;
@@ -297,9 +313,10 @@ fn compact_leaf(og: &[u8], new: &mut [u8]) -> CompactResult {
                 // insup
                 let key_len = u32::from_be_bytes(og[i + 1..i + 5].try_into().unwrap()) as usize;
                 let val_len = u32::from_be_bytes(og[i + 5..i + 9].try_into().unwrap()) as usize;
-                let key = &og[i + 9..i + 9 + key_len];
-                let val = &og[i + 9 + key_len..i + 9 + key_len + val_len];
-                entries.insert(key, val);
+                scratch.push((
+                    i + 9..i + 9 + key_len,
+                    i + 9 + key_len..i + 9 + key_len + val_len,
+                ));
                 i += 9 + key_len + val_len;
             }
             _ => panic!(),
@@ -312,10 +329,10 @@ fn compact_leaf(og: &[u8], new: &mut [u8]) -> CompactResult {
     //
     // for the header/footer, everything is zeroed, so we only need to write in
     // the number of entries
-    new[1..3].copy_from_slice(&(entries.len() as u16).to_be_bytes());
+    new[1..3].copy_from_slice(&(scratch.len() as u16).to_be_bytes());
     let mut top_i = 3;
     let mut bottom_i = new.len() - 1;
-    for (key, val) in &entries {
+    for (key, val) in scratch.iter() {
         // copy in the new key and value at the bottom, and update the cursor
         new[bottom_i - val.len()..bottom_i].copy_from_slice(val);
         new[bottom_i - (val.len() + key.len())..bottom_i - val.len()].copy_from_slice(key);
@@ -337,14 +354,16 @@ fn compact_leaf(og: &[u8], new: &mut [u8]) -> CompactResult {
 
     // then we check if we need to split
     if top < SPLIT_POINT {
-        return CompactResult::NeedsSplit(top, entries.len() / 2);
+        return CompactResult::NeedsSplit(top, scratch.len() / 2);
     }
 
     CompactResult::Ok(top)
 }
 
 fn search_inner(page_buf: &[u8], key: &[u8]) -> PageId {
-    let mut entries = BTreeMap::new();
+    // TODO: so instead of just putting stuff in a btree, we should just keep a "best option so
+    // far" variable, and update it as necessary
+    let mut best = None;
     let mut i = 0;
     loop {
         match page_buf[i] {
@@ -366,16 +385,27 @@ fn search_inner(page_buf: &[u8], key: &[u8]) -> PageId {
                             .try_into()
                             .unwrap(),
                     );
-                    entries.insert(k, page_id);
+                    if key <= k {
+                        match best {
+                            Some((b_k, b_pid)) => {
+                                if k < b_k {
+                                    return page_id;
+                                } else {
+                                    return b_pid;
+                                }
+                            }
+                            None => return page_id,
+                        }
+                    }
                     cursor -= key_len;
                 }
-                for (k, pid) in entries {
-                    if key <= k {
-                        return pid;
+
+                match best {
+                    Some((_, pid)) => return pid,
+                    None => {
+                        return u64::from_be_bytes(page_buf[i + 3..i + 3 + 8].try_into().unwrap());
                     }
                 }
-
-                return u64::from_be_bytes(page_buf[i + 3..i + 3 + 8].try_into().unwrap());
             }
             1 => {
                 // insert or update
@@ -383,7 +413,16 @@ fn search_inner(page_buf: &[u8], key: &[u8]) -> PageId {
                     u32::from_be_bytes(page_buf[i + 1..i + 5].try_into().unwrap()) as usize;
                 let k = &page_buf[i + 5 + 8..i + 5 + 8 + key_len];
                 let page_id = u64::from_be_bytes(page_buf[i + 5..i + 5 + 8].try_into().unwrap());
-                entries.insert(k, page_id);
+                if key <= k {
+                    match best {
+                        Some((b_k, _)) => {
+                            if k < b_k {
+                                best = Some((k, page_id));
+                            }
+                        }
+                        None => best = Some((k, page_id)),
+                    }
+                }
                 i += 5 + 8 + key_len;
             }
             _ => panic!(),
